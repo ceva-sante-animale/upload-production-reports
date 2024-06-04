35c725ecc2ef25b6296a41d76c9e569060b7d1f5
Chaker KHACHINE <146932757+chaker-git@users.noreply.github.com>
2024-06-04 13:26:43 +0200

    retrieve file namle from file

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import logging
+import re
+
 import functions_framework
 from flask import Request
 
+
 @functions_framework.http
-def hello_http(request):
+def hello_http(request: Request):
     """HTTP Cloud Function.
     Args:
         request (flask.Request): The request object.
@@ -12,16 +16,15 @@ def hello_http(request):
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
-    request_json = request.get_json(silent=True)
-    request_args = request.args
+    if not request.files.get('files'):
+        logging.error(f"Received a {request.method} request with no file attached...")
 
-    print(request.get_json(silent=True))
-
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
-    else:
-        name = 'World'
-    return 'Salut {} . Je suis la cloud function upload_prouction_report !'.format(name)
-
+    filename = request.files.get('files').filename
+    logging.debug(f"Received a {request.method} request with {filename=}")
+    client_name = filename.split("_")[0]
+    args = re.findall(r"(\d+)", filename)
+    print(filename)
+    print(args)
+    print(client_name)
+    print(request.form)
+    return {}
