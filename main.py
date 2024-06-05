60e5d9468b1cd135338d6c44401f773d75dc1037
Chaker KHACHINE <146932757+chaker-git@users.noreply.github.com>
2024-06-05 15:39:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import logging
 import os
 import time
-from functools import cache
 from functools import lru_cache
+from typing import List
 
 import functions_framework
 from flask import Request
@@ -11,12 +11,10 @@ from google.cloud.storage import Client
 
 is_production = bool(os.environ.get("PORT"))
 
-
-@cache
-def get_gcs_client() -> Client:
-    if not is_production:
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
-    return Client()
+if not is_production:
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
+BUCKET_NAME = "ecat_id_test"
+bucket = Client().bucket(BUCKET_NAME)
 
 
 def get_ttl_hash(seconds=3600):
@@ -26,16 +24,11 @@ def get_ttl_hash(seconds=3600):
 
 # cache will be updated once in an hour
 @lru_cache()
-def get_api_keys(ttl_hash=None):
+def get_api_keys(ttl_hash=None) -> List[str]:
     logging.debug(f"Loading api keys from Firestore and cache it for one hour ...")
     store = firestore.Client()
     ref = store.collection("apikey")
     return [doc.get().get("apikey") for doc in ref.list_documents()]
-
-
-BUCKET_NAME = "ecat_id_test"
-storage = get_gcs_client()
-bucket = storage.bucket(BUCKET_NAME)
 
 
 @functions_framework.http
@@ -49,12 +42,14 @@ def upload_file(request: Request):
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
+    file = request.files.get('file')
+
     if request.method.lower() != "post":
         error = f"Received a {request.method} request, but we only accept POST requests"
         logging.error(error)
         return {"error": error}, 405
 
-    if not request.files.get('file'):
+    if not file:
         error = f"Received a {request.method} request with no file attached..."
         logging.error(error)
         return {"error": error}, 400
@@ -63,11 +58,11 @@ def upload_file(request: Request):
     if len(metadata) != 3 or len(metadata[-1].split("-")) != 3:
         error = (
             f"The request url must include exactly the client id, machine code and date following this format "
-            f"'/client_id/machine_code/date/' (/3328/PMXA-C32432/2024-02-22/) but provided path is {request.path}"
+            f"'/client_id/machine_code/date[dd-mm-yyyy]/' (/3328/PMXA-C32432/22-05-2024/) "
+            f"but the provided path is {request.path}"
         )
         logging.error(error)
         return {"error": error}, 400
-    [client_id, machine_code, date] = metadata
 
     keys = get_api_keys(ttl_hash=get_ttl_hash())
     apikey = request.args.get('apikey')
@@ -76,18 +71,18 @@ def upload_file(request: Request):
         logging.error(error)
         return {"error": error}, 401
 
-    file = request.files.get('file')
-    filename = file.filename
+    [client_id, machine_code, date] = metadata
+    [d, m, y] = date.split("-")
     try:
-        blob = bucket.blob(file.filename)
+        blob = bucket.blob(f"{client_id}/{machine_code}/{y}/{m}/{d}/{file.filename}")
         blob.upload_from_file(file)
         logging.debug(
-            f"Successfully upload file {filename=} for {client_id=} {machine_code=} {date=}"
+            f"Successfully upload file {file.filename=} for {client_id=} {machine_code=} {date=}"
         )
         return {"success": True}, 201
     except Exception as exc:
         logging.error(
-            f"Not able to upload file {filename=} for {client_id=} {machine_code=} {date=}"
+            f"Not able to upload file {file.filename=} for {client_id=} {machine_code=} {date=}"
         )
-        logging.error(f"Error returned by the package: {str(exc)}")
+        logging.error(f"Error returned by GC storage: {str(exc)}")
         return {"success": False}, 400
